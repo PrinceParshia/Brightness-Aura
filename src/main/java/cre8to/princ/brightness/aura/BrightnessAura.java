8a3e0579fb4fa2aeabfb4d997bb8cc90e2b5b900
PrinceParshia <princeparshiajr@gmail.com>
2025-01-28 19:37:24 +0600

    .

--- src/main/java/cre8to/princ/brightness/aura/BrightnessAura.java
+++ src/main/java/cre8to/princ/brightness/aura/BrightnessAura.java
@@ -1,6 +1,8 @@
 package cre8to.princ.brightness.aura;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
@@ -9,6 +11,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import org.lwjgl.glfw.GLFW;
 
+@Environment(EnvType.CLIENT)
 public class BrightnessAura implements ClientModInitializer {
 	private static KeyBinding brightnessKey;
 	private static boolean isBrightnessMax = false;
@@ -27,7 +30,7 @@ public class BrightnessAura implements ClientModInitializer {
 				if (isBrightnessMax) {
 					client.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
 				} else {
-					client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, Integer.MAX_VALUE, 0, false, false, false));
+					client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, StatusEffectInstance.INFINITE, 0, false, false, false));
 				}
 				isBrightnessMax = !isBrightnessMax;
 			}
