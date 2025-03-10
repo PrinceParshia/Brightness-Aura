164f9d5b8d5199f0a4939cb2488e698cf820c8eb
PrinceParshia <princeparshiajr@gmail.com>
2025-03-10 16:25:06 +0600

    .

--- src/main/java/cre8to/princ/brightness/aura/BrightnessAura.java
+++ src/main/java/cre8to/princ/brightness/aura/BrightnessAura.java
@@ -4,35 +4,59 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class BrightnessAura implements ClientModInitializer {
-	private static boolean isBrightnessMax = true;
-	private static boolean wasKeyPressed = false;
+	private static double originalGamma = 0.5;
+	private static final double maxGamma = 15.0;
+	private static final int delay = 20;
+	private double targetGamma = 0.5;
+	private boolean transition = false;
+	private double stepSize;
+	private int stepsRemaining;
+	private boolean wasKeyPressed = false;
 
-	private static final KeyMapping brightnessKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-					"key.brightness-aura.brightness-key",
+	private static final KeyMapping brightnessAuraKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+					"key.brightness.aura",
 					InputConstants.Type.KEYSYM,
 					GLFW.GLFW_KEY_B,
-					"category.brightness-aura"
+					"key.categories.aura"
 			)
 	);
 
 	@Override
 	public void onInitializeClient() {
-	}
-
-	public static boolean isBrightnessMax() {
-		if (brightnessKey.isDown()) {
-			if (!wasKeyPressed) {
-				isBrightnessMax = !isBrightnessMax;
-				wasKeyPressed = true;
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (brightnessAuraKey.isDown() && !wasKeyPressed) {
+				if (!transition) {
+					if (client.options.gamma().get() != maxGamma) {
+						originalGamma = client.options.gamma().get();
+						targetGamma = maxGamma;
+					} else {
+						targetGamma = originalGamma;
+					}
+					stepsRemaining = delay;
+					stepSize = (targetGamma - client.options.gamma().get()) / stepsRemaining;
+					transition = true;
+				}
 			}
-		} else { wasKeyPressed = false; }
 
-		return isBrightnessMax;
+			if (transition) {
+				if (stepsRemaining <= 0) {
+					client.options.gamma().set(targetGamma);
+					transition = false;
+				} else {
+					double currentGamma = client.options.gamma().get();
+					client.options.gamma().set(currentGamma + stepSize);
+					stepsRemaining--;
+				}
+			}
+
+			wasKeyPressed = brightnessAuraKey.isDown();
+		});
 	}
 }
--- src/main/java/cre8to/princ/brightness/aura/mixin/UnitDoubleMixin.java
+++ src/main/java/cre8to/princ/brightness/aura/mixin/UnitDoubleMixin.java
@@ -2,14 +2,16 @@ package cre8to.princ.brightness.aura.mixin;
 
 import net.minecraft.client.OptionInstance;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
 @Mixin(OptionInstance.UnitDouble.class)
-public abstract class UnitDoubleMixin<T> {
-    @Overwrite
-    public Optional<T> validateValue(T object) {
-        return Optional.of(object);
+public abstract class UnitDoubleMixin {
+    @Inject(method = "validateValue", at = @At("HEAD"), cancellable = true)
+    public void validateValue(Double object, CallbackInfoReturnable<Optional<Double>> cir) {
+        cir.setReturnValue(Optional.of(object));
     }
 }
